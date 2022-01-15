1c572a99e611bd5037ffbe7f343da4db0f9dccb3
David Alexis <david@umbracorp.cl>
2022-01-14 21:43:37 -0300

    quiero llorar , funcona

--- src/apollo_conect.js
+++ src/apollo_conect.js
@@ -4,7 +4,7 @@ import {
   InMemoryCache,
   gql,
   HttpLink,
-	split
+  split,
 } from "@apollo/client";
 
 import { setContext } from "@apollo/client/link/context";
@@ -13,6 +13,47 @@ import { createAuthLink } from "aws-appsync-auth-link";
 
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
+import { v4 as uuidv4 } from "uuid";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import * as gPrinter from "graphql/language/printer";
+
+const createAppSyncGraphQLOperationAdapter = () => ({
+  applyMiddleware: async (options, next) => {
+    // AppSync expects GraphQL operation to be defined as a JSON-encoded object in a "data" property
+    options.data = JSON.stringify({
+      query:
+        typeof options.query === "string"
+          ? options.query
+          : gPrinter.print(options.query),
+      variables: options.variables,
+    });
+
+    // AppSync only permits authorized operations
+    options.extensions = { authorization: h };
+
+    // AppSync does not care about these properties
+    delete options.operationName;
+    delete options.variables;
+    // Not deleting "query" property as SubscriptionClient validation requires it
+
+    next();
+  },
+});
+class UUIDOperationIdSubscriptionClient extends SubscriptionClient {
+  generateOperationId() {
+    // AppSync recommends using UUIDs for Subscription IDs but SubscriptionClient uses an incrementing number
+    return uuidv4();
+  }
+  processReceivedData(receivedData) {
+    try {
+      const parsedMessage = JSON.parse(receivedData);
+      if (parsedMessage?.type === "start_ack") return; // sent by AppSync but meaningless to us
+    } catch (e) {
+      throw new Error("Message must be JSON-parsable. Got: " + receivedData);
+    }
+    super.processReceivedData(receivedData);
+  }
+}
 
 const endpoint =
   "https://we2zjvgvrzbfdnzh6wihtx3gnq.appsync-api.us-east-2.amazonaws.com/graphql";
@@ -42,13 +83,26 @@ const headerBase64 = Buffer.from(JSON.stringify(h)).toString("base64");
 //const payloadBase64 = btoa(JSON.stringify(p));
 const payloadBase64 = btoa(JSON.stringify(p));
 const uriwss = `${endpointwss}?header=${headerBase64}&payload=${payloadBase64}`;
-console.log(uriwss);
-  const wsLink = new WebSocketLink({
-    uri: uriwss,
-    options: {
-      reconnect: true,
-    },
-  });
+const wsLink = new WebSocketLink({
+  uri: uriwss,
+  options: {
+    reconnect: true,
+  },
+});
+
+	const wsLink2 = new WebSocketLink(
+	  new UUIDOperationIdSubscriptionClient(
+	    uriwss,
+	    {
+	      timeout: 5 * 60 * 1000,
+	      reconnect: true,
+	      lazy: true,
+	      connectionCallback: (err) =>
+	        console.log("connectionCallback", err ? "ERR" : "OK", err || ""),
+	    },
+	    WebSocket
+	  ).use([createAppSyncGraphQLOperationAdapter()])
+	);
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
@@ -69,7 +123,6 @@ const apolloClient = () => {
   return client;
 };
 const apolloCLientWss = () => {
-
   //  const authLink = setContext((_, { headers }) => {
   //    return {
   //      headers: {
@@ -82,7 +135,7 @@ const apolloCLientWss = () => {
 
   const client = new ApolloClient({
     // link: authLink.concat(wsLink),
-    link: splitLink,
+    link: wsLink2,
     cache: new InMemoryCache(),
   });
 
